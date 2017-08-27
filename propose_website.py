79da3b220be06e6fc13d83de2c0336f5c97079e4
futuremaze <future.maze@gmail.com>
2017-08-27 10:27:18 +0900

    [add] 音声認識Julius呼び出し用のテストプログラム作成

---
+++ propose_website.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import socket
+# import xml.etree.ElementTree as ET
+
+
+def main():
+    host = 'localhost'
+    port = 10500
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(host, port)
+
+    try:
+        data = ''
+        while True:
+            if '</RECOGOUT>\n.' in data:
+                print data
+                data = ''
+                # root = ET.fromstring(
+                #     '<?xml version="1.0"?>\n' + data[data.find(
+                #         '<RECOGOUT>'):].replace('\n.', ''))
+                # for whypo in root.findall('./SHYPO/WHYPO'):
+                #     command = whypo.get('WORD')
+                #     score = float(whypo.get('CM'))
+                #
+                #     if command == u'おはよう' and score >= 0.9:
+                #         # ここにおはよう処理
+                #
+                # data = ''
+
+            else:
+                data = data + client.recv(1024)
+    except KeyboardInterrupt:
+        client.close()
+
+
+if __name__ == "__main__":
+    main()
