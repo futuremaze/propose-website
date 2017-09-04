080037c3f9ac6e834b1570eca89f30950450ccce
futuremaze <future.maze@gmail.com>
2017-09-05 08:29:15 +0900

    [add,update] テスト用のWebSocket サーバ&クライアントを作成

---
+++ propose_website.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+
+cl = []
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        if self not in cl:
+            cl.append(self)
+
+    def on_message(self, message):
+        for client in cl:
+            client.write_message(message)
+
+    def on_close(self):
+        if self in cl:
+            cl.remove(self)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('test_ws.html')
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+    (r"/websocket", WebSocketHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(10080)
+    tornado.ioloop.IOLoop.current().start()
---
+++ test_client.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys
+from websocket import create_connection
+ws = create_connection("ws://localhost:10080/websocket")
+
+if len(sys.argv) > 1:
+    message = sys.argv[1]
+else:
+    message = 'hello world!'
+
+print(ws.send(message))
+print(ws.recv())
+
+ws.close()
+
